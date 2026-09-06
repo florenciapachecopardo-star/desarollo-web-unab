819fd158254837e4e040ac0ebdc333a45a4c5a33
Karla M. <k.muozlen@uandresbello.edu>
2026-09-06 20:18:16 -0300

    Delete Semana5/Semana5/graphql-usuarios directory

--- Semana5/Semana5/graphql-usuarios/models/usuario.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const usuarioSchema = new mongoose.Schema({
-    nombre: { type: String, required: true },
-    pass: { type: String, required: true }
-});
-
-module.exports = mongoose.model("Usuario", usuarioSchema);
